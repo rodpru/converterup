18a39636746a3658b7ea881dca3b749f8cf5c806
rodpru <rgp.prt@gmail.com>
2025-12-02 14:09:33 +0000

    build

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,6 +8,21 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: '50mb',
     },
+  },
+  transpilePackages: ['@nutrient-sdk/viewer'],
+  compiler: {
+    removeConsole: false,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
   },
 };
 
